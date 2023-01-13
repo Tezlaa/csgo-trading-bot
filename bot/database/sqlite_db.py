f9a91a97189dced6868ac5fb0522299386b61432
Bogdan <95874565+Tezla1992@users.noreply.github.com>
2023-01-13 23:47:59 +0200

    del file db

--- bot/database/sqlite_db.py
+++ bot/database/sqlite_db.py
@@ -14,12 +14,14 @@ async def sql_start():
     else:
         logging.info(base)
         
-    base.execute('CREATE TABLE IF NOT EXISTS profile(user_id TEXT PRIMARY KEY, name TEXT, balance INT)')
+    base.execute('CREATE TABLE IF NOT EXISTS profile(\
+ user_id TEXT PRIMARY KEY, name TEXT, balance INTEGER, admin INTEGER\
+ )')
     base.commit()
 
 
 async def create_profile(user_id, username):
     user = cur.execute(f'SELECT 1 FROM profile WHERE user_id == {user_id}').fetchone()
     if not user:
-        cur.execute("INSERT INTO profile VALUES(?, ?, ?)", (user_id, username, 15))
+        cur.execute("INSERT INTO profile VALUES(?, ?, ?, ?)", (user_id, username, 15, 0))
         base.commit()
